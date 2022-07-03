bc5e07f9c014860cd373dcd3acebd9b203207f0a
utopia7 <tguchn@gmail.com>
2022-07-03 16:49:58 +0800

    4.7.5

--- docker_crack.sh
+++ docker_crack.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo "docker for emby 4.7.4"
+echo "docker for emby 4.7.5"
 echo "-------------------web crack-------------------"
 rm -f /system/Emby.Web.dll
-wget -P /system/ https://act.jiawei.xin:10086/tmp/emby/4.7.4.0/linux64_docker64_4.7.4/Emby.Web.dll
+wget -P /system/ https://raw.githubusercontent.com/utopia7/stdrep/main/Emby.Web.dll
 rm -f /system/MediaBrowser.Model.dll
-wget -P /system/ https://act.jiawei.xin:10086/tmp/emby/4.7.4.0/linux64_docker64_4.7.4/MediaBrowser.Model.dll
+wget -P /system/ https://raw.githubusercontent.com/utopia7/stdrep/main/MediaBrowser.Model.dll
 echo "core crack done..."
 rm -f /system/dashboard-ui/modules/emby-apiclient/connectionmanager.js
-wget -P /system/dashboard-ui/modules/emby-apiclient/ https://act.jiawei.xin:10086/tmp/emby/4.7.4.0/linux64_docker64_4.7.4/dashboard-ui/modules/emby-apiclient/connectionmanager.js
+wget -P /system/dashboard-ui/modules/emby-apiclient/ https://raw.githubusercontent.com/utopia7/stdrep/main/dashboard-ui/modules/emby-apiclient/connectionmanager.js
 rm -f /system/dashboard-ui/embypremiere/embypremiere.js
-wget -P /system/dashboard-ui/embypremiere/ https://act.jiawei.xin:10086/tmp/emby/4.7.4.0/linux64_docker64_4.7.4/dashboard-ui/embypremiere/embypremiere.js
+wget -P /system/dashboard-ui/embypremiere/ https://raw.githubusercontent.com/utopia7/stdrep/main/dashboard-ui/embypremiere/embypremiere.js
 echo "web crack done..."
 rm -f /system/Emby.Server.Implementations.dll
-wget -P /system/ https://act.jiawei.xin:10086/tmp/emby/4.7.4.0/linux64_docker64_4.7.4/Emby.Server.Implementations.dll
+wget -P /system/ https://raw.githubusercontent.com/utopia7/stdrep/main/Emby.Server.Implementations.dll
 echo "Implementations replace authentication..."
 
 echo "replace done..."
